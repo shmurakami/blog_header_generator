4b681d5367948bdabcdbce755aaf2851a626e392
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-04-06 14:21:42 +0900

    return errors

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/shmurakami/blog_header_generator/engine"
 )
 
-func main() {
+func generate() error {
 	// TODO get output path from option
 
 	jekyll := engine.New()
@@ -16,26 +17,46 @@ func main() {
 	// seems golang does not support dynamic value get/set without reflection
 	// for now support only jekyll
 
-	jekyll.Title = ask("Title", jekyll.Title)
-	jekyll.Description = ask("Description", jekyll.Description)
-	jekyll.Date = ask("Date", jekyll.Date)
-	jekyll.Filename = ask("Filename", jekyll.Filename)
+	var err error
+	if jekyll.Title, err = ask("Title", jekyll.Title); err != nil {
+		return err
+	}
+	if jekyll.Description, err = ask("Description", jekyll.Description); err != nil {
+		return err
+	}
+	if jekyll.Date, err = ask("Date", jekyll.Date); err != nil {
+		return err
+	}
+	if jekyll.Filename, err = ask("Filename", jekyll.Filename); err != nil {
+		return err
+	}
 
-	err := jekyll.Output("./_posts")
+	err = jekyll.Output("./_posts")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	fmt.Println("File generated")
+	return nil
 }
 
-func ask(t, initial string) string {
+func main() {
+	err := generate()
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+}
+
+func ask(t, initial string) (string, error) {
 	stdin := ""
 	fmt.Printf("%s? (%s): ", t, initial)
-	fmt.Scanln(&stdin)
+	_, err := fmt.Scanln(&stdin)
+	if err != nil {
+		return "", err
+	}
 
 	if stdin == "" {
-		return initial
+		return initial, nil
 	}
-	return stdin
+	return stdin, nil
 }
